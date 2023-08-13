01f966c11c4d8ba9baa61ec714ff276d26eeb439
Adam <adam.aji2007@gmail.com>
2023-08-13 13:08:06 +0700

    Initial commit

---
+++ lib/HomePage.dart
@@ -0,0 +1,23 @@
+import 'package:belajar_terbaru/Film.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
